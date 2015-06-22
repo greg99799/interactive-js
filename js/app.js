08054f47cabb0ba8a5bccc832b167d0c8472febc
Greg <greg.knudsen09@gmail.com>
2015-06-22 11:21:50 -0400

    add skeleton functions and variables

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 //Problem: User interaction doesn't provide desired rseults
 //Solution: Add interactivity so user can manage daily tasks
 
+var taskInput = document.getElementById("new-task"); //new task
+var addButton = document.getElementsByTagName("button")[0]; //first button
+var incompleteTaskHolder = document.getElementById("incomplete-tasks"); //incomplete tasks
+var completedTaskHolder = document.getElementById("completed-tasks"); //completed tasks
+
 //Add a new task
 var addTask = function() {
   //when button is pressed
@@ -13,6 +18,7 @@ var addTask = function() {
       //each elements, need modified and appended
 }
 //Edit existing task
+var editTask = function() {
   //When edit button is pressed
     //if class of parent(li) is .edit
       //switch from .editMode
@@ -22,17 +28,22 @@ var addTask = function() {
       //input value becomes label's text
 
     //toggle .editMode on parent
-
+}
 
 //Delete existing task
+var deleteTask = function() {
   //When delete button is pressed
     //remove parent <li> from <ul>
+}
 
 //Mark task as complete
+var taskCompleted = function() {
   //When the checkbox is checked
     //append the task <li> to #completed-tasks
-
+}
 
 //Mark task as incomplete
+var taskIncomplete = function() {
   //When the checkbox is unchecked
   //append the task <li> to #incomplete-tasks
+}
